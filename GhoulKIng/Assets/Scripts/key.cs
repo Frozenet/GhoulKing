e994eecfd9fa8e585d99a092d4461161e106be5b
Alex chavez <a.geovanny.jr@gmail.com>
2022-08-08 15:41:38 -0400

    audio

--- GhoulKIng/Assets/Scripts/key.cs
+++ GhoulKIng/Assets/Scripts/key.cs
@@ -12,9 +12,9 @@ public class key : MonoBehaviour
     }
     public void OnTriggerEnter(Collider other)
     {
+
         if (other.CompareTag("Player"))
         {
-            gameManager.instance.updateKeyNumber();
             Destroy(gameObject);
         }
         
